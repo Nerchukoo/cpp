14dedb5e3913024621e60aedbfdbf5ffd848cd1d
Koraveni Srikanth <22618308+koraveni@users.noreply.github.com>
2022-08-04 12:10:38 +0530

    Add arrays

---
+++ basics/arrays.cpp
@@ -0,0 +1,17 @@
+int main() {
+    
+    // Declare an array of char type with known size
+    char a[5];
+
+    // Declare an array of int type with known size
+    int b[5];
+
+    // Declare and initialize an array in single statement
+    int c[6] = { 1, 2, 3, 5, 7, 11};
+
+    // Modify 4th element in the above array
+    c[3] = 13;
+
+    return 0;
+}
+
